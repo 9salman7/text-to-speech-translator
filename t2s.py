c1ab7c1c399d4ed233cf46c2ef0cc7bc99a259ca
Salman <salman97@gmail.com>
2019-08-16 17:42:09 +0530

    Update t2s.py
    added comments

--- t2s.py
+++ t2s.py
@@ -1,20 +1,18 @@
-# Import the required module for text  
-# to speech conversion 
-from gtts import gTTS 
-from playsound import playsound
-from googletrans import Translator
+from gtts import gTTS #module for text to speech
+from playsound import playsound #module to play audop
+from googletrans import Translator #google translate api for python 
 import os 
    
-language = 'en'
+language = 'en' #output language
 
 translator=Translator()
 
 inputText=input("Enter text to translate to English\n");
 
-transText=translator.translate(inputText).text
+transText=translator.translate(inputText).text #translated text
   
-myobj = gTTS(text=transText, lang=language, slow=False) 
+myobj = gTTS(text=transText, lang=language, slow=False) #converted text
 
-myobj.save("speech.mp3") 
+myobj.save("speech.mp3") #save in mp3
   
-playsound('speech.mp3')
+playsound('speech.mp3') #play mp3 file
